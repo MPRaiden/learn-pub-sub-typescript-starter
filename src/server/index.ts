5a4fa4b89b9cc3b5200aab7a9fe49b8294eca191
mpr <marinpericradan1@gmail.com>
2025-11-22 17:52:32 +0100

    feat: initial logic to create/close RabitMQ connections

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,6 +1,22 @@
+import amqp from 'amqplib'
+
 async function main() {
-  console.log("Starting Peril server...");
+  const connectionStr = "amqp://guest:guest@localhost:5672/"
+  const connection = await amqp.connect(connectionStr)
+  console.log("Successful connected to RabbitMQ")
+
+  process.on("SIGINT", async () => {
+    try {
+      await connection.close()
+      console.log("Stopping connection to RabbitMQ")
+    } catch (error) {
+      console.error("Error while closing connection to RabbitMQ", error)
+    } finally {
+      process.exit(0);
+    }
+  })
 }
+
 
 main().catch((err) => {
   console.error("Fatal error:", err);
