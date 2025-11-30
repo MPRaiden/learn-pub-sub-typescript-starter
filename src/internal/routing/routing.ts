14f590318696ca045f9357bd1bac3f2406edee5d
mpr <marinpericradan1@gmail.com>
2025-11-30 20:20:26 +0100

    feat: repl for taking in user input & publishing of pause and resume messages

--- src/internal/routing/routing.ts
+++ src/internal/routing/routing.ts
@@ -1,6 +1,7 @@
 export const ArmyMovesPrefix = "army_moves";
 export const WarRecognitionsPrefix = "war";
 export const PauseKey = "pause";
+export const ResumeKey = "resume";
 export const GameLogSlug = "game_logs";
 
 export const ExchangePerilDirect = "peril_direct";
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,17 +1,13 @@
 import amqp from 'amqplib'
+import { getInput, printServerHelp } from '../internal/gamelogic/gamelogic.js'
 import { publishJSON } from '../internal/pubsub/publish.js'
-import { ExchangePerilDirect, PauseKey } from '../internal/routing/routing.js'
+import { ExchangePerilDirect, PauseKey, ResumeKey } from '../internal/routing/routing.js'
 
 async function main() {
   const connectionStr = "amqp://guest:guest@localhost:5672/"
   const connection = await amqp.connect(connectionStr)
-  console.log("Successful connected to RabbitMQ")
 
-  const confirmChannel = await connection.createConfirmChannel()
-  const ps = {
-    isPaused: true
-  }
-  await publishJSON(confirmChannel, ExchangePerilDirect, PauseKey, ps)
+  printServerHelp()
 
   process.on("SIGINT", async () => {
     try {
@@ -20,13 +16,51 @@ async function main() {
     } catch (error) {
       console.error("Error while closing connection to RabbitMQ", error)
     } finally {
-      process.exit(0);
+      process.exit(0)
     }
   })
+
+  const confirmChannel = await connection.createConfirmChannel()
+
+  while (true) {
+    const userInput = await getInput()
+
+    if (userInput.length === 0) continue
+
+    const firstWord = userInput[0]
+
+    if (firstWord === "pause") {
+      console.log("Pause message sent")
+      const ps = {
+        isPaused: true
+      }
+      try {
+        await publishJSON(confirmChannel, ExchangePerilDirect, PauseKey, ps)
+      } catch (err) {
+        console.error(`Error publishing Pause message -> ${err}`)
+      }
+    } else if (firstWord === "resume") {
+      console.log("Resume message sent")
+      const ps = {
+        isPaused: false
+      }
+      try {
+        await publishJSON(confirmChannel, ExchangePerilDirect, ResumeKey, ps)
+      } catch (err) {
+        console.error(`Error publishing Resume message -> ${err}`)
+      }
+    } else if (firstWord === "quit") {
+      console.log("Exiting server")
+      process.exit(0)
+    } else {
+      console.log("Unknown command")
+    }
+  }
 }
 
 
 main().catch((err) => {
-  console.error("Fatal error:", err);
-  process.exit(1);
-});
+  console.error("Fatal error:", err)
+  process.exit(1)
+})
+
