c043be31c3d8653f702a319b1701e00a9abcd615
mpr <marinpericradan1@gmail.com>
2025-11-30 18:38:05 +0100

    created basic connection on client side & new binding function helper

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,5 +1,12 @@
+import amqp from 'amqplib'
+import { clientWelcome } from '../internal/gamelogic/gamelogic.js';
+
 async function main() {
+  const clientConnStr = "amqp://guest:guest@localhost:5672/"
+  const connection = await amqp.connect(clientConnStr)
   console.log("Starting Peril client...");
+
+  const username = await clientWelcome()
 }
 
 main().catch((err) => {
---
+++ src/internal/pubsub/consume.ts
@@ -0,0 +1,34 @@
+import amqp, { Channel } from 'amqplib'
+
+export type SimpleQueueType = "durable" | "transient";
+
+/**
+ *Creates a new queue and binds it to an exchange.
+
+ @param {amqp.ChannelModel} conn - TCP connection between node and RabbitMQ.
+ @param {string} exchange - Exchange where routing occurs.
+ @param {string} queueName - Name of the queue where the exchange routes the messages sent.
+ @param {string} key - Routing key.
+ @param {SimpleQueueType} queueType - Either durable or transient.
+*/
+export async function declareAndBind(
+  conn: amqp.ChannelModel,
+  exchange: string,
+  queueName: string,
+  key: string,
+  queueType: SimpleQueueType,
+): Promise<[Channel, amqp.Replies.AssertQueue]> {
+
+  const channel = await conn.createConfirmChannel()
+
+  const durable = true ? queueType === "durable" : false
+  const autoDelete = true ? queueType === "transient" : false
+  const exclusive = true ? queueType === "transient" : false
+
+  const queue = await channel.assertQueue(queueName, { durable: durable, autoDelete: autoDelete, exclusive: exclusive })
+
+  channel.bindQueue(queueName, exchange, key)
+
+  return [channel, queue]
+}
+
